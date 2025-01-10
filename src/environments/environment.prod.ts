4dc6ac6c36a06d9b1159d6d799df4e8ec31727f7
AldoXhelilaj <aldoxhelili69@hotmail.com>
2025-01-10 18:22:26 +0100

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://my-json-server.typicode.com/AldoXhelilaj/file-manager',
+    apiUrl: 'https://file-manager.vercel.app/',
 };
